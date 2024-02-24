863e83b369419f52051dbd9affbdb58e413ebd04
ItsNotCam <camallanyoung@gmail.com>
2024-02-23 20:44:03 -0500

    home page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import "../styles/globals.css";
-import { ok } from "./test"
+import $ from 'jquery'
 
-console.log(`hello ${ok()} haha nice`)
+$( "#haha" ).on( "click", function() {
+  console.log( $( this ).text() );
+});
+
+$( "li" ).on("click", function() {
+	console.log($(this).text())
+})
+
+registerMutualExclusion(".title-wrapper li", document.querySelectorAll(".title-wrapper li"));
+registerMutualExclusion(".img-selector", document.querySelectorAll(".img-selector"));
+
+
+function registerMutualExclusion(selector: string, items: any): void {
+	$( selector ).on("click", function() {
+		$(this).addClass("selected")
+		items.forEach((item: any) => {
+			if(item !== this) {
+				$(item).removeClass("selected")
+			}
+		})
+	})
+}
