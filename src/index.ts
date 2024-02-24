66bd99807da06d96fd453634a85db0f771dfef8b
ItsNotCam <camallanyoung@gmail.com>
2024-02-23 21:49:49 -0500

    moved media query to bottom, added image selector transition

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,29 @@ import $ from 'jquery'
 registerMutualExclusion(".title-wrapper li", document.querySelectorAll(".title-wrapper li"));
 registerMutualExclusion(".img-selector", document.querySelectorAll(".img-selector"));
 
+const imgs = [
+	$("#img-number-0"),
+	$("#img-number-1"),
+	$("#img-number-2")
+]
+
+$( ".img-selector" ).on("click", function() {
+	const idx = parseInt(this.id.replace("img-select-", ""));
+	for(let i = 0; i < imgs.length; i++) {
+		if(idx === i) {
+			imgs[i].removeClass("hidden")
+		} else {
+			imgs[i].addClass("hidden")
+		}
+	}
+})
+
 function registerMutualExclusion(selector: string, items: any): void {
 	$( selector ).on("click", function() {
-		$(this).addClass("selected")
+		$(this).addClass("selected");
 		items.forEach((item: any) => {
 			if(item !== this) {
-				$(item).removeClass("selected")
+				$(item).removeClass("selected");
 			}
 		})
 	})
