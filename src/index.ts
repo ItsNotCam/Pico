a589089b2d9b25ed1518b1fc73d03f70adbda497
ItsNotCam <camallanyoung@gmail.com>
2024-02-24 15:21:14 -0500

    transitions and animations

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import "../styles/globals.css";
 import $ from 'jquery'
 
-registerMutualExclusion(".title-wrapper li", document.querySelectorAll(".title-wrapper li"));
-registerMutualExclusion(".img-selector", document.querySelectorAll(".img-selector"));
+const imgs = [ $("#img-number-0"), $("#img-number-1"), $("#img-number-2") ]
+const selectors = [ ".hero-btn-container button", "#primary-nav li" ]
 
-const imgs = [
-	$("#img-number-0"),
-	$("#img-number-1"),
-	$("#img-number-2")
-]
+registerMutualExclusion(
+	".title-wrapper li",
+	document.querySelectorAll(".title-wrapper li")
+);
 
-$( ".img-selector" ).on("click", function() {
+registerMutualExclusion(
+	".img-selector",
+	document.querySelectorAll(".img-selector")
+);
+
+selectors.forEach(selector => {
+	$(selector).on("click", function() { redirect(this) })
+})
+
+$(".img-selector").on("click", function() {
 	const idx = parseInt(this.id.replace("img-select-", ""));
 	for(let i = 0; i < imgs.length; i++) {
 		if(idx === i) {
@@ -21,8 +29,27 @@ $( ".img-selector" ).on("click", function() {
 	}
 })
 
+function redirect(item: any): void {
+	const link = $(item).data("link")
+	console.log(link, window.location.pathname);
+
+
+	if(window.location.pathname === link) {
+		return;
+	}
+
+	$(function() {
+		const $newDiv = $('<div/>', { class: "page-out" })
+		$('body').prepend($newDiv)
+	})
+
+	setTimeout(() => {
+		window.location.assign(link);
+	}, 500)
+}
+
 function registerMutualExclusion(selector: string, items: any): void {
-	$( selector ).on("click", function() {
+	$(selector).on("click", function() {
 		$(this).addClass("selected");
 		items.forEach((item: any) => {
 			if(item !== this) {
