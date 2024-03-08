83d48448fbc0a2324a18d36b2640dc2f554bdd97
ItsNotCam <camallanyoung@gmail.com>
2024-03-07 23:12:57 -0500

    i forgot haha

--- src/purchase.ts
+++ src/purchase.ts
@@ -1,27 +1,57 @@
-import $ from 'jquery';
-import { registerMutualExclusions } from './util';
+import $ from "jquery";
 
-const opt1 = $("#pb-opt-list-model > div");
-opt1.each(function() { 
-  $(this).on("click", function() {
-    opt1.each(function() { $(this).attr("data-selected", "false") })
+const optModel = $("#pb-opt-list-model > div");
+const optStorage = $("#pb-opt-list-storage > div");
+
+optModel.each(function () {
+  $(this).on("click", function () {
+    optModel.each(function () {
+      $(this).attr("data-selected", "false");
+    });
     $(this).attr("data-selected", "true");
-  })
-})
+  });
+});
 
-const opt2 = $("#pb-opt-list-color > div");
-opt2.each(function() { 
-  $(this).on("click", function() {
-    opt2.each(function() { $(this).attr("data-selected", "false") })
+optStorage.each(function () {
+  $(this).on("click", function () {
+    optStorage.each(function () {
+      $(this).attr("data-selected", "false");
+    });
     $(this).attr("data-selected", "true");
-  })
-})
+  });
+});
 
-const opt3 = $("#pb-opt-list-storage > div");
-opt3.each(function() { 
-  $(this).on("click", function() {
-    opt3.each(function() { $(this).attr("data-selected", "false") })
-    $(this).attr("data-selected", "true");
-  })
-})
+enum COLOR {
+  LIGHT,
+  DARK,
+}
 
+var COLOR_STATE: COLOR = COLOR.DARK;
+
+const clrLight: JQuery = $("#pb-color-option-light");
+const clrDark: JQuery = $("#pb-color-option-dark");
+const clrTxt: JQuery = $("#color-choice");
+
+clrLight.on("click", () => {
+  $(clrLight).attr("data-selected", "true");
+  $(clrDark).attr("data-selected", "false");
+
+  $(clrTxt).html("light.");
+  $(clrTxt).removeClass("clr-1");
+  $(clrTxt).removeClass("clr-6");
+  $(clrTxt).addClass("clr-1");
+
+  COLOR_STATE = COLOR.LIGHT;
+});
+
+clrDark.on("click", () => {
+  $(clrLight).attr("data-selected", "false");
+  $(clrDark).attr("data-selected", "true");
+
+  $(clrTxt).html("dark.");
+  $(clrTxt).removeClass("clr-1");
+  $(clrTxt).removeClass("clr-6");
+  $(clrTxt).addClass("clr-6");
+
+  COLOR_STATE = COLOR.DARK;
+});
