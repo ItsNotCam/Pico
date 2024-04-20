52bf528458ba6f9c4b9b3f2b326362557d621345
ItsNotCam <camallanyoung@gmail.com>
2024-04-20 15:24:38 -0400

    Script to deploy to mason servers and home servers

---
+++ deploy_home.sh
@@ -0,0 +1,4 @@
+npx webpack
+scp *.html cam@192.168.1.3:~/projects/pico
+scp -r public cam@192.168.1.3:~/projects/pico
+scp -r components cam@192.168.1.3:~/projects/pico
---
+++ deploy_mason.sh
@@ -0,0 +1,4 @@
+npx webpack
+scp *.html cyoung35@mason.gmu.edu:/home/u6/cyoung35/public_html
+scp -r public cyoung35@mason.gmu.edu:/home/u6/cyoung35/public_html
+scp -r components cyoung35@mason.gmu.edu:/home/u6/cyoung35/public_html
