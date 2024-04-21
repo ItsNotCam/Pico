09d626ab9208df2ad0b0a59f7cc1aa3fdc7cf5a2
ItsNotCam <camallanyoung@gmail.com>
2024-04-21 17:25:59 -0400

    finished discover/nano page

--- src/purchase.ts
+++ src/purchase.ts
@@ -1,16 +1,39 @@
 import $ from "jquery";
 
-const purchaseImgSelectors = [$("#img-select-1"), $("#img-select-2"), $("#img-select-3")];
-const purchaseImgs = [$("#product-image-1"), $("#product-image-2"), $("#product-image-3")]
-for(let i = 0; i < purchaseImgSelectors.length; i++) {
-  purchaseImgSelectors[i].on("click", () => {
-    for(let j = 0; j < purchaseImgSelectors.length; j++) {
-      let isSelected = j === i ? "true" : "false";
-      purchaseImgSelectors[j].attr("data-selected", isSelected);
-      purchaseImgs[j].attr("data-visible", isSelected);
+
+// dynamic image selectors
+// bad implementation but it works for this small scale project
+const selectors: JQuery<HTMLElement> = $('div[id]').filter(function() {
+  const regex = /^img-select-(\d{1,})/;
+  return regex.test(this.id);
+})
+const imgs: JQuery<HTMLElement> = $('*[id]').filter(function() {
+  const regex = /^product-image-(\d{1,})/;
+  return regex.test(this.id);
+});
+
+for(let i = 0; i < selectors.length; i++) {
+  $(selectors[i]).on("click", () => {
+    for(let j = 0; j < selectors.length; j++) {
+      let isSelected = j == i ? "true" : "false";
+      $(selectors[j]).attr("data-selected", isSelected);
+      $(imgs[j]).attr("data-visible", isSelected);
     }
-  })
+  });
 }
+
+// hard coded image selectors
+// const purchaseImgSelectors = [$("#img-select-1"), $("#img-select-2"), $("#img-select-3")];
+// const purchaseImgs = [$("#product-image-1"), $("#product-image-2"), $("#product-image-3")]
+// for(let i = 0; i < purchaseImgSelectors.length; i++) {
+//   purchaseImgSelectors[i].on("click", () => {
+//     for(let j = 0; j < purchaseImgSelectors.length; j++) {
+//       let isSelected = j === i ? "true" : "false";
+//       purchaseImgSelectors[j].attr("data-selected", isSelected);
+//       purchaseImgs[j].attr("data-visible", isSelected);
+//     }
+//   })
+// }
 
 const optModel = $("#pb-opt-list-model > div");
 const optStorage = $("#pb-opt-list-storage > div");
